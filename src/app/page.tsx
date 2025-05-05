b979b5db8bc8d6ebbcbbc97446dac8d79aa2671a
pengamatpisangkeju <satria.ndoy1710@gmail.com>
2025-05-05 13:23:17 +0700

    change home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,9 @@
-import Button from "@/components/Button";
-import FormField from "@/components/form/FormField";
-import Input from "@/components/form/Input";
-import Select from "@/components/form/Select";
-import Textarea from "@/components/form/Textarea";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex justify-center">
-      <div className="grid gap-8 p-4 w-xl">
-        <h1 className="text-3xl font-semibold">Registrasi</h1>
-        <form className="grid gap-4">
-          <FormField label="Nama Lengkap">
-            <Input />
-          </FormField>
-
-          <FormField label="Nama Ibu">
-            <Input />
-          </FormField>
-
-          <FormField label="Tanggal Lahir">
-            <Input type="date" />
-          </FormField>
-
-          <FormField label="Email">
-            <Input type="email" />
-          </FormField>
-
-          <FormField label="No. Handphone">
-            <Input />
-          </FormField>
-
-          <FormField label="Alamat">
-            <Textarea />
-          </FormField>
-
-          <FormField label="Provinsi">
-            <Select options={[]} />
-          </FormField>
-
-          <FormField label="Kecamatan">
-            <Select options={[]} />
-          </FormField>
-
-          <FormField label="Kelurahan">
-            <Select options={[]} />
-          </FormField>
-
-          <FormField label="RT">
-            <Input />
-          </FormField>
-
-          <FormField label="RW">
-            <Input />
-          </FormField>
-
-          <FormField label="Kode POS">
-            <Select options={[]} />
-          </FormField>
-
-          <Button type="submit" text="Submit"/>
-        </form>
-      </div>
-    </div>
+    <Link href="/form-registrasi">
+    <h1>Go to Register Form</h1>
+    </Link>
   )
 }
